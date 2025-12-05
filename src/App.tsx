7b0bc077367706844c4c5ba29b5b2e00d9518bcc
pponikiewski <p.ponikiewski7@gmail.com>
2025-12-06 00:39:28 +0100

    feat: initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '2rem' }}>
+      <h1>Róża</h1>
+    </div>
+  )
+}
+
+export default App
