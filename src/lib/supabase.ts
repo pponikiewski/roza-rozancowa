4ca91431466dcb4d58ee22c33c6a07fa43b07dc9
pponikiewski <p.ponikiewski7@gmail.com>
2025-12-10 20:49:58 +0100

    refactor: remove debug logs from Supabase client initialization and clarify utility function documentation

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,13 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 
-// --- DEBUG ---
-console.log("Supabase URL:", supabaseUrl)
-console.log("Supabase Key:", supabaseKey)
-// -------------
-
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('Brakuje zmiennych środowiskowych! Sprawdź plik .env.local')
 }
 
+// Inicjalizacja klienta Supabase do komunikacji z bazą danych
 export const supabase = createClient(supabaseUrl, supabaseKey)
