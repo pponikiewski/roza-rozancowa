8258460ac1b5a4b5346f7fc16a4ae6ded316e1e7
pponikiewski <p.ponikiewski7@gmail.com>
2025-12-10 20:49:58 +0100

    refactor: remove debug logs from Supabase client initialization and clarify utility function documentation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Funkcja pomocnicza do łączenia klas CSS (Tailwind) z obsługą warunków i konfliktów
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
